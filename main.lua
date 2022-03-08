34655a6c0663ed772b8a86752e076583f6356a48
Gabe <gabrield3d@outlook.com>
2022-03-07 20:58:38 -0500

    Added basic example

---
+++ main.lua
@@ -0,0 +1,17 @@
+
+
+ogmo = require("ogmo")
+
+function love.load()
+
+	level_0 = ogmo.read_map("maps/level0.json", love.graphics.newImage("tilesets/0x72_16x16RobotTileset.v1.png"))
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+	level_0:draw()
+end
--- ogmo.lua
+++ ogmo.lua
@@ -1,132 +1,3 @@
---
--- ogmo.lua
---
--- Copyright (c) 2020 elGabe
---
--- Permission is hereby granted, free of charge, to any person obtaining a copy of
--- this software and associated documentation files (the "Software"), to deal in
--- the Software without restriction, including without limitation the rights to
--- use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
--- of the Software, and to permit persons to whom the Software is furnished to do
--- so, subject to the following conditions:
---
--- The above copyright notice and this permission notice shall be included in all
--- copies or substantial portions of the Software.
---
--- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
--- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
--- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
--- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
--- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
--- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
--- SOFTWARE.
---
-
-local ogmo = {}
-
-local add = table.insert
-
--- Reads an ogmo json file and takes in a texture to assign to the level
-function ogmo.read_map(path, texture)
-    local map = {}
-    map.texture = texture
-    local string = love.filesystem.read(path)
-    map.data = json.decode(string)
-
-    map.tiles_layer = {}
-    map.entities = {}
-    map.width = map.data.width
-    map.height = map.data.height
-    map.solids = {}
-
-    -- Loop through all layers
-    for l = 1, #map.data.layers do
-        -- Get current layer
-        local layer = map.data.layers[l]
-        
-        -- Check if this is an entity layer
-        if layer.entities ~= nil then
-            map.entities = layer.entities
-        end
-        
-        -- Check if layer has data
-        if layer.data ~= nil then
-            add(map.tiles_layer, layer)
-        end
-
-        if (layer.data2D ~= nil) then
-            --[TODO] Unpack data2D into 1D array and run through same process
-        end
-
-        -- This layer represents solids
-        if (string.lower(layer.name) == "solids") then
-            map.solids = layer.data
-        end
-    end
-
-    -- Information to split the texture
-    local cell_width = map.data.layers[1].gridCellWidth
-    local cell_height = map.data.layers[1].gridCellHeight
-    local grid_width = map.data.layers[1].gridCellsX
-    local grid_height = map.data.layers[1].gridCellsY
-    local texture_width = map.texture:getWidth()
-    local texture_height = map.texture:getHeight()
-
-    map.subimages = {}
-
-    -- locals instead of globals for performance
-    local love_draw = love.graphics.draw
-    local love_quad = love.graphics.newQuad
-
-    -- Splitting texture into individual tile images
-    for uvy = 0, (texture_height / cell_height) - 1 do
-        for uvx = 0, (texture_width / cell_width) - 1 do
-            local quad = love_quad(uvx * cell_width, uvy * cell_height, cell_width, cell_height, map.texture:getDimensions())
-            add(map.subimages, quad)
-        end
-    end
-
-function map:draw()
-
-    -- Loop through the tiles to draw
-    for l = #map.tiles_layer, 1, -1 do
-        local layer = map.tiles_layer[l]
-
-        for y = 0, grid_height-1 do
-            for x = 0, grid_width-1 do
-                local tile = layer.data[(y * grid_width + x) + 1]
-                local xx = cell_width * x
-                local yy = cell_height * y
-
-                if (tile ~= -1) then
-                    love_draw(map.texture, map.subimages[tile+1], xx, yy)
-                end
-            end
-        end
-    end
-
-end
-
-function map:draw_layer(layer_index)
-    local layer = map.tiles_layer[layer_index]
-
-    for y = 0, grid_height-1 do
-        for x = 0, grid_width-1 do
-            local tile = layer.data[(y * grid_width + x) + 1]
-            local xx = cell_width * x
-            local yy = cell_height * y
-
-            if (tile ~= -1) then
-                love_draw(map.texture, map.subimages[tile+1], xx, yy)
-            end
-        end
-    end
-end
-
-    return map
-end
-
-return ogmo
 
 --
 -- json.lua
@@ -514,5 +385,132 @@ function json.decode(str)
   return res
 end
 
+--
+-- ogmo.lua
+--
+-- Copyright (c) 2020 elGabe
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy of
+-- this software and associated documentation files (the "Software"), to deal in
+-- the Software without restriction, including without limitation the rights to
+-- use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+-- of the Software, and to permit persons to whom the Software is furnished to do
+-- so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in all
+-- copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+--
 
-return json
+local ogmo = {}
+
+local add = table.insert
+
+-- Reads an ogmo json file and takes in a texture to assign to the level
+function ogmo.read_map(path, texture)
+    local map = {}
+    map.texture = texture
+    local string = love.filesystem.read(path)
+    map.data = json.decode(string)
+
+    map.tiles_layer = {}
+    map.entities = {}
+    map.width = map.data.width
+    map.height = map.data.height
+    map.solids = {}
+
+    -- Loop through all layers
+    for l = 1, #map.data.layers do
+        -- Get current layer
+        local layer = map.data.layers[l]
+        
+        -- Check if this is an entity layer
+        if layer.entities ~= nil then
+            map.entities = layer.entities
+        end
+        
+        -- Check if layer has data
+        if layer.data ~= nil then
+            add(map.tiles_layer, layer)
+        end
+
+        if (layer.data2D ~= nil) then
+            --[TODO] Unpack data2D into 1D array and run through same process
+        end
+
+        -- This layer represents solids
+        if (string.lower(layer.name) == "solids") then
+            map.solids = layer.data
+        end
+    end
+
+    -- Information to split the texture
+    local cell_width = map.data.layers[1].gridCellWidth
+    local cell_height = map.data.layers[1].gridCellHeight
+    local grid_width = map.data.layers[1].gridCellsX
+    local grid_height = map.data.layers[1].gridCellsY
+    local texture_width = map.texture:getWidth()
+    local texture_height = map.texture:getHeight()
+
+    map.subimages = {}
+
+    -- locals instead of globals for performance
+    local love_draw = love.graphics.draw
+    local love_quad = love.graphics.newQuad
+
+    -- Splitting texture into individual tile images
+    for uvy = 0, (texture_height / cell_height) - 1 do
+        for uvx = 0, (texture_width / cell_width) - 1 do
+            local quad = love_quad(uvx * cell_width, uvy * cell_height, cell_width, cell_height, map.texture:getDimensions())
+            add(map.subimages, quad)
+        end
+    end
+
+function map:draw()
+
+    -- Loop through the tiles to draw
+    for l = #map.tiles_layer, 1, -1 do
+        local layer = map.tiles_layer[l]
+
+        for y = 0, grid_height-1 do
+            for x = 0, grid_width-1 do
+                local tile = layer.data[(y * grid_width + x) + 1]
+                local xx = cell_width * x
+                local yy = cell_height * y
+
+                if (tile ~= -1) then
+                    love_draw(map.texture, map.subimages[tile+1], xx, yy)
+                end
+            end
+        end
+    end
+
+end
+
+function map:draw_layer(layer_index)
+    local layer = map.tiles_layer[layer_index]
+
+    for y = 0, grid_height-1 do
+        for x = 0, grid_width-1 do
+            local tile = layer.data[(y * grid_width + x) + 1]
+            local xx = cell_width * x
+            local yy = cell_height * y
+
+            if (tile ~= -1) then
+                love_draw(map.texture, map.subimages[tile+1], xx, yy)
+            end
+        end
+    end
+end
+
+    return map
+end
+
+return ogmo
